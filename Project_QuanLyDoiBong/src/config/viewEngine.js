5a492c32772a69de4846e05449b120afc0d318e9
Le Duy Quan <leduyquan2574@gmail.com>
2025-05-22 11:33:12 +0700

    commit Duy Quan

--- Project_QuanLyDoiBong/src/config/viewEngine.js
+++ Project_QuanLyDoiBong/src/config/viewEngine.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const express = require('express');
 const configViewEngine = (app) => {
-    //config ejs
     app.set('view engine', 'ejs');
-    app.set('views', "../src" + '/views'); // Set the views directory
+    app.set('views', "../src" + '/views'); 
     // app.use(express.static('public'));
     app.use(express.static(path.join("./src" , 'public'))); // Set the static files directory
 }
